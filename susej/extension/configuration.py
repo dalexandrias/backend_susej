19cf36784d0b066bfe14a572e51941f30ef21dca
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-05-21 00:19:37 -0300

    Atualizando

--- susej/extension/configuration.py
+++ susej/extension/configuration.py
@@ -3,6 +3,11 @@ from flask import Flask
 
 
 def init_app(app: Flask) -> None:
+    """Realiza a configuração da aplicação, carregando as variaveis de ambiente
+
+    Args:
+        app (Flask): application Flask
+    """
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('HEROKU_POSTGRESQL_BLUE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 

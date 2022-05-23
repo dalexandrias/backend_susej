f57913608f446941dbf390c0b4f58694b26057f6
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-05-23 00:03:01 -0300

    Atualizando inicializacao app

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from susej.model import auth_model as auth_migrate
 from susej.model import client_model as client_migrate
 
 
-def create_app():
-    app = Flask(__name__)
-    configuration.init_app(app)
-    database.init_app(app)
-    login_resource.init_app(app)
-    product_resource.init_app(app)
-    client_resource.init_app(app)
-    
-    # Atualiza as alterações das tabelas de dados
-    auth_migrate.init_app(app)
-    client_migrate.init_app(app)
-    
-    return app
+# def create_app():
+app = Flask(__name__)
+configuration.init_app(app)
+database.init_app(app)
+login_resource.init_app(app)
+product_resource.init_app(app)
+client_resource.init_app(app)
+
+# Atualiza as alterações das tabelas de dados
+auth_migrate.init_app(app)
+client_migrate.init_app(app)
+
+# return app

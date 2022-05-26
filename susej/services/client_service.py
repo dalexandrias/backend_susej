0051c34f0aa3d9ebef3a903a876038e36a5aa31e
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-05-25 23:53:07 -0300

    Criado function update_client com regras de validação para atualização do objetos e melhorado a criação do objeto Client no save_client

--- susej/services/client_service.py
+++ susej/services/client_service.py
@@ -2,8 +2,8 @@ from http import HTTPStatus
 import json
 
 import validators
-from susej.commands_dml.commands import commit_session, get_all_client, get_one_by_email, insert_table, select_table
-from susej.constants.status_return import CREATED_CLIENT, EMAIL_ALREADY_EXISTS, EMAIL_INVALID, ERROR, SUCCESS
+from susej.commands_dml.commands import commit_session, get_all_client, get_one_by_email, insert_table, get_one_by_id_client, update_client
+from susej.constants.status_return import CREATED_CLIENT, EMAIL_ALREADY_EXISTS, EMAIL_INVALID, ERROR, ID_NOT_FOUND, SUCCESS, UPDATE_CLIENT
 from susej.model.client_model import Client, ClientAddress
 
 
@@ -13,15 +13,14 @@ class ClientService(object):
     Args:
         object (_type_): _description_
     """
+
     def __init__(self) -> None:
         pass
 
     def save_client(self, client_body: json) -> json:
 
-        client_address = [ClientAddress(**client_address)
-                          for client_address in client_body['address']]
-        client_in = Client(nome=client_body['nome'], sobrenome=client_body['sobrenome'],
-                           email=client_body['email'], address=client_address)
+        client_in = Client(**client_body)
+        # client_address_in = ClientAddress.parsing(client_body['address_data'])
 
         client_email = get_one_by_email(client_in)
         if not client_email:
@@ -47,3 +46,33 @@ class ClientService(object):
 
     def get_all_client(self):
         return get_all_client()
+    
+    def update_client(self, client_body: json, id: int) -> json:
+        
+        client = get_one_by_id_client(id)
+        if client:
+            if validators.email(client_body['email']):
+                    client_address = client_body['address_data']
+                    del client_body['address_data']
+                    update_client(Client, client_body, id)
+                    
+                    for address in client_address:
+                        id_address = address['id']
+                        update_client(ClientAddress, address, id_address)
+                        
+                    commit_session()
+
+                    return {
+                        "status": SUCCESS,
+                        "message": UPDATE_CLIENT
+                    }, HTTPStatus.CREATED
+            else:
+                return {
+                    "status": ERROR,
+                    "message": EMAIL_INVALID
+                }, HTTPStatus.BAD_REQUEST
+        else:
+            return {
+                    "status": ERROR,
+                    "message": ID_NOT_FOUND
+                }, HTTPStatus.BAD_REQUEST

6cf5e5983bb65dbb0e5394c2e10c4002099f5784
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-05-25 23:46:44 -0300

    Criado a function update_client para atualização dos objetos

--- susej/commands_dml/commands.py
+++ susej/commands_dml/commands.py
@@ -1,3 +1,5 @@
+import json
+from typing import Any
 from susej.extension.database import db
 from susej.model.client_model import Client, ClientAddress
 
@@ -18,6 +20,9 @@ def delete_table(all=False, **args) -> None:
 
     db.session.commit()
     db.session.close()
+    
+def update_client(table: object, values: dict, id: int) -> None:
+    db.session.query(table).filter(table.id == id).update(values, synchronize_session='fetch')
 
 
 def commit_session() -> None:
@@ -25,7 +30,10 @@ def commit_session() -> None:
     db.session.close()
 
 def get_all_client() -> list[Client]:
-    return db.session.query(Client).join(Client.address).all()
+    return db.session.query(Client).join(Client.address_data).all()
 
 def get_one_by_email(table: object):
     return table.query.filter_by(email=table.email).first()
+
+def get_one_by_id_client(id: int) -> Client:
+    return db.session.query(Client).get(id)

f94cef53b3ff76c7038a2ea9125349550dbb03bc
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-06-01 22:24:34 -0300

    Adicionado delete_client, realiza o delete do client

--- susej/services/client_service.py
+++ susej/services/client_service.py
@@ -2,8 +2,9 @@ from http import HTTPStatus
 import json
 
 import validators
-from susej.commands_dml.commands import commit_session, get_all_client, get_one_by_email, insert_table, get_one_by_id_client, update_client
-from susej.constants.status_return import CREATED_CLIENT, EMAIL_ALREADY_EXISTS, EMAIL_INVALID, ERROR, ID_NOT_FOUND, SUCCESS, UPDATE_CLIENT
+from susej.exceptions.error import AuthError
+from susej.commands_dml.commands import commit_session, delete_one_by_id_client, get_all_client, get_one_by_email, insert_table, get_one_by_id_client, update_client
+from susej.constants.status_return import CREATED_CLIENT, DELETE_CLIENT, EMAIL_ALREADY_EXISTS, EMAIL_INVALID, ERROR, ID_NOT_FOUND, SUCCESS, UPDATE_CLIENT
 from susej.model.client_model import Client, ClientAddress
 
 
@@ -20,7 +21,6 @@ class ClientService(object):
     def save_client(self, client_body: json) -> json:
 
         client_in = Client(**client_body)
-        # client_address_in = ClientAddress.parsing(client_body['address_data'])
 
         client_email = get_one_by_email(client_in)
         if not client_email:
@@ -33,39 +33,30 @@ class ClientService(object):
                     "message": CREATED_CLIENT
                 }, HTTPStatus.CREATED
             else:
-                return {
-                    "status": ERROR,
-                    "message": EMAIL_INVALID
-                }, HTTPStatus.BAD_REQUEST
+                raise AuthError(EMAIL_INVALID)
 
         else:
-            return {
-                "status": ERROR,
-                "message": EMAIL_ALREADY_EXISTS
-            }, HTTPStatus.BAD_REQUEST
+            raise AuthError(EMAIL_ALREADY_EXISTS)
 
-    def get_all_client(self):
-        return get_all_client()
-    
     def update_client(self, client_body: json, id: int) -> json:
-        
+
         client = get_one_by_id_client(id)
         if client:
             if validators.email(client_body['email']):
-                    client_address = client_body['address_data']
-                    del client_body['address_data']
-                    update_client(Client, client_body, id)
-                    
-                    for address in client_address:
-                        id_address = address['id']
-                        update_client(ClientAddress, address, id_address)
-                        
-                    commit_session()
+                client_address = client_body['address_data']
+                del client_body['address_data']
+                update_client(Client, client_body, id)
 
-                    return {
-                        "status": SUCCESS,
-                        "message": UPDATE_CLIENT
-                    }, HTTPStatus.CREATED
+                for address in client_address:
+                    id_address = address['id']
+                    update_client(ClientAddress, address, id_address)
+
+                commit_session()
+
+                return {
+                    "status": SUCCESS,
+                    "message": UPDATE_CLIENT
+                }, HTTPStatus.CREATED
             else:
                 return {
                     "status": ERROR,
@@ -73,6 +64,17 @@ class ClientService(object):
                 }, HTTPStatus.BAD_REQUEST
         else:
             return {
-                    "status": ERROR,
-                    "message": ID_NOT_FOUND
-                }, HTTPStatus.BAD_REQUEST
+                "status": ERROR,
+                "message": ID_NOT_FOUND
+            }, HTTPStatus.BAD_REQUEST
+
+    def get_all_client(self) -> list[Client]:
+        return get_all_client()
+
+    def delete_client(self, id: int) -> json:
+        delete_one_by_id_client(id)
+        commit_session()
+        return {
+            "status": SUCCESS,
+            "message": DELETE_CLIENT
+        }, HTTPStatus.CREATED
